8182772181c9a56662089474605820738d22c25c
Giacomo Nanni <gn.nanni@gmail.com>
2023-12-23 18:32:25 +0100

    fix path

--- src/components/Detail.svelte
+++ src/components/Detail.svelte
@@ -21,7 +21,7 @@
         <div>{@html $selectedBall?.text}</div>
     {/if}
     <div>
-        <a href="{base}{base == "" ? "/archive" : "archive.html"}">Boring view?</a>
+        <a href="{base}{base == "" ? "/archive" : "/archive.html"}">Boring view?</a>
         <h1>Simina German</h1>
         <div>{@html info.text}</div>
     </div>
